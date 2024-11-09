e0d1024ad5133e47282dea5fd15fa4fd20af150d
Tom-com-ar <tomasiezzi16@gmail.com>
2024-11-09 13:28:53 -0300

    Clase 9/11

---
+++ JavaScriptFlex/2.Funcionamiento/script.js
@@ -0,0 +1,43 @@
+// console.log("Hola")
+
+// let nombre = "Tomás\n"
+
+// console.log(nombre)
+
+// let edad = 16
+
+// let mayoredad = edad >=18
+
+// console.log(mayoredad)
+
+// let perronombre 
+// perronombre= "Nick"
+
+// console.log(perronombre)
+
+// const PI = 3.1416
+// const URL = "www.google.com"
+
+
+// let nombre = prompt("¿Como te llamas?")
+
+// alert("Bienvenido" + nombre + "¿Como Andas?")
+
+// let confirmacion = confirm("¿Quieres Seguir navegando?")
+
+// let edad = Number(prompt("¿Cuantos años tienes?"))
+
+// console.log(isNaN(edad))
+
+// console.log(edad + 4)
+// console.log(100)
+
+//Calculadora de imc
+
+let peso = Number(prompt("Introdusca tu peso en kilogramos: "))
+
+let altura = Number(prompt("Introdusca tu altura en metros: "))
+
+let imc = peso / (altura * altura)
+
+alert("Tu indice de masa corporal es: " + imc.toFixed(2))
